3aaee92f362c052c29271369dee23a9de91e5579
Michael Samuel Clark III <121131570+Valydendor3000@users.noreply.github.com>
2025-12-06 22:11:58 -0500

    Update vite.config.ts

--- admin-root/vite.config.ts
+++ admin-root/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'dist' }
+  publicDir: 'public',   // default is 'public' but set it explicitly to be sure
+  build: { outDir: 'dist' } // default is 'dist'; match your Pages "Build output directory"
 })
