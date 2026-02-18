737ac8ae5524e6ab315880961c5a2e67daa98911
Michael Samuel Clark III <121131570+Valydendor3000@users.noreply.github.com>
2026-02-18 14:46:54 -0500

    Update app.config.ts

--- tripstack-starter/tripstack/apps/mobile/app.config.ts
+++ tripstack-starter/tripstack/apps/mobile/app.config.ts
@@ -1,10 +1,11 @@
-export default () => ({
-  expo: {
-    name: 'TripStack',
-    slug: 'tripstack',
-    scheme: 'tripstack',
-    extra: {
-      EXPO_PUBLIC_API_URL: process.env.APP_BASE_URL || 'https://your-worker.workers.dev'
-    }
-  }
+export default ({ config }) => ({
+  ...config,
+  name: "TripStack",
+  slug: "tripstack",
+  extra: {
+    EXPO_PUBLIC_API_URL: process.env.APP_BASE_URL || 'https://your-worker.workers.dev',
+    eas: {
+      projectId: "8f38cff2-c06a-4e85-be79-2af5d0f79190",
+    },
+  },
 });
