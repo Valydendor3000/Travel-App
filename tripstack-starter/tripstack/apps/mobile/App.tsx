6afdff8329491c2552c14cfc00e9fa7ee706218e
Michael Samuel Clark III <121131570+Valydendor3000@users.noreply.github.com>
2026-02-21 07:09:39 -0500

    Add files via upload

---
+++ tripstack-starter/tripstack/apps/mobile/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>TripStack Mobile</Text>
+      <Text>Dev client is working 🎉</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "600",
+    marginBottom: 10,
+  },
+});
